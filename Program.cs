4e59c32386210b239712ebe67120b68dc699cee2
Cliff Allderige <ca.allderige@gmail.com>
2019-03-11 09:14:01 -0400

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+using static Pokedex.Pokemon;
+
+namespace Pokedex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world.");
+        }
+    }
+}
